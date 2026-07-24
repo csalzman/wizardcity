cabdd8290bc3b28439ec7fdfa5f587d61c02e0dd
Chris <csalzman@gmail.com>
2026-07-24 19:14:28 -0400

    Lots (#29)
    Co-authored-by: Christopher Salzman <christopher.salzman@gmail.com>

--- src/spellbook/cells.ts
+++ src/spellbook/cells.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "../db/databaseconnect";
 const cellsRoutes = express.Router();
+import { cellsWithRegionStmt } from "./statements";
 
 // Update a cell
 cellsRoutes.post("/cell/:cell_id", async (req: any, res: any) => {
@@ -47,6 +48,33 @@ cellsRoutes.post("/cell/:cell_id", async (req: any, res: any) => {
 
   res.write(
     `event: datastar-patch-elements\ndata: elements <div id="map-messages">Clicked: ${req.params.cell_id}</div>\n\n`,
+  );
+
+  res.end();
+});
+
+// Get a cell
+cellsRoutes.get("/cell-sidebar/:cell_id", async (req: any, res: any) => {
+  // Get cell
+  const cellId = req.params.cell_id;
+  const getCellStmt = await db.prepare(cellsWithRegionStmt);
+  const cell = await getCellStmt.get([cellId]);
+
+  // If no map found return early
+  if (!cell) {
+    res.send("<p>No Cell</p>");
+    return res.end();
+  }
+
+  const htmlSnippet: any = await new Promise((resolve, reject) => {
+    res.render("partials/cellSidebar", { cell }, (err: any, html: any) => {
+      if (err) reject(err);
+      else resolve(html);
+    });
+  });
+
+  res.write(
+    `event: datastar-patch-elements\ndata: elements ${htmlSnippet.replace(/\n/g, "")}\n\n`,
   );
 
   res.end();
